15297ae15a31a67fad75af123935f3db9d897375
Alois Aebischer PC perso <alois.aebischer24@gmail.com>
2018-09-19 15:34:44 +0200

    Keep both display sync

--- js/directives/message/message.controller.js
+++ js/directives/message/message.controller.js
@@ -6,7 +6,7 @@
 
     function messageCtrl($scope, $element) {
         $scope.displayPreMessage = false;
-        var message = $element.find("input");
+        //Structure of the HTML file that is sent to the client
         var htmlContent1 = "<!DOCTYPE html>\r\n";
         htmlContent1 += "<html>\r\n";
         htmlContent1 += "<head>\r\n";
@@ -19,9 +19,21 @@
         htmlContent2 += "<script type='text/javascript' src='main.js'></script>\r\n";
         htmlContent2 += "</html>\r\n";
         var htmlContent = null;
-        message.on('keyup', function (e) {
-            if (e.keyCode == 13) {
-                htmlContent = htmlContent1 + "<h1 class='message'>" + message[0].value + "</h1>\r\n" + htmlContent2;
+        var messageText = document.getElementsByClassName("message-text");
+        //Function to call when modifying the input
+        for (var i = 0; i < messageText.length; i++) {
+            if(i==0){
+                messageText[i].addEventListener('keyup', updateTargetDown);
+            }
+            else{
+                messageText[i].addEventListener('keyup', updateTargetUp);
+            }
+        }
+        //Update top input when bottom input is modified + send message when enter
+        function updateTargetUp(event) {
+            if(event.keyCode==13){
+                messageText[0].value=messageText[1].value;
+                htmlContent = htmlContent1 + "<h1 class='message'>" + messageText[1].value + "</h1>\r\n" + htmlContent2;
                 fs.writeFile('Raspberry/raspberry-web/index.html', htmlContent, function (err) {
                     if (err) throw err;
                     else {
@@ -30,12 +42,34 @@
                     }
                 })
             }
-        })
+            else{
+                messageText[0].value=messageText[1].value;
+            }
+        }
+        //Update bottom input when top input is modified + send message when enter
+        function updateTargetDown(event) {
+            if(event.keyCode==13){
+                messageText[1].value=messageText[0].value;
+                htmlContent = htmlContent1 + "<h1 class='message'>" + messageText[0].value + "</h1>\r\n" + htmlContent2;
+                fs.writeFile('Raspberry/raspberry-web/index.html', htmlContent, function (err) {
+                    if (err) throw err;
+                    else {
+                        ssh.execCommand('/home/pi/Desktop/refresh.sh', { cwd: '/var/www' }).then(function (result) {
+                        })
+                    }
+                })
+            }
+            else{
+                messageText[1].value=messageText[0].value;
+            }
+        }
+        //Function to display/hide emoticons
         $scope.displayListEmoFunc = function (value) {
             var emoticonsList = $element.find(".emoticons-list");
             if (value) emoticonsList.show();
             else emoticonsList.hide();
         }
+        //Function to write HTML file with happy emoticon + refresh the web page on the client
         $scope.printHappy = function () {
             htmlContent = htmlContent1 + "<div class='message-image-happy'></div>\r\n" + htmlContent2;
             fs.writeFile('Raspberry/raspberry-web/index.html', htmlContent, function (err) {
@@ -46,6 +80,7 @@
                 }
             });
         }
+        //Function to write HTML file with sad emoticon + refresh the web page on the client
         $scope.printSad = function () {
             htmlContent = htmlContent1 + "<div class='message-image-sad'></div>\r\n" + htmlContent2;
             fs.writeFile('Raspberry/raspberry-web/index.html', htmlContent, function (err) {
@@ -56,6 +91,7 @@
                 }
             });
         }
+        //Function to write HTML file with nice emoticon + refresh the web page on the client
         $scope.printNice = function () {
             htmlContent = htmlContent1 + "<div class='message-image-nice'></div>\r\n" + htmlContent2;
             fs.writeFile('Raspberry/raspberry-web/index.html', htmlContent, function (err) {
@@ -66,6 +102,7 @@
                 }
             });
         }
+        //Function to write HTML file with pacman emoticon + refresh the web page on the client
         $scope.printPacman = function () {
             htmlContent = htmlContent1 + "<div class='message-image-pacman'></div>\r\n" + htmlContent2;
             fs.writeFile('Raspberry/raspberry-web/index.html', htmlContent, function (err) {
@@ -76,6 +113,7 @@
                 }
             });
         }
+        //Function to write HTML file with hublot emoticon + refresh the web page on the client
         $scope.printHublot = function () {
             htmlContent = htmlContent1 + "<div class='message-image-hublot'></div>\r\n" + htmlContent2;
             fs.writeFile('Raspberry/raspberry-web/index.html', htmlContent, function (err) {
@@ -86,6 +124,7 @@
                 }
             });
         }
+        //Function to write HTML file with shark emoticon + refresh the web page on the client
         $scope.printShark = function () {
             htmlContent = htmlContent1 + "<div class='message-image-shark'></div>\r\n" + htmlContent2;
             fs.writeFile('Raspberry/raspberry-web/index.html', htmlContent, function (err) {
@@ -96,6 +135,7 @@
                 }
             });
         }
+        //Function to write HTML file with minion emoticon + refresh the web page on the client
         $scope.printMinion = function () {
             htmlContent = htmlContent1 + "<div class='message-image-minion'></div>\r\n" + htmlContent2;
             fs.writeFile('Raspberry/raspberry-web/index.html', htmlContent, function (err) {
@@ -106,6 +146,7 @@
                 }
             });
         }
+        //Function to write HTML file with minion emoticon + refresh the web page on the client
         $scope.printMinion2 = function () {
             htmlContent = htmlContent1 + "<div class='message-image-minion2'></div>\r\n" + htmlContent2;
             fs.writeFile('Raspberry/raspberry-web/index.html', htmlContent, function (err) {
@@ -116,6 +157,7 @@
                 }
             });
         }
+        //Function to write HTML file with bubble emoticon + refresh the web page on the client
         $scope.printBubble = function () {
             htmlContent = htmlContent1 + "<div class='message-image-bubble'></div>\r\n" + htmlContent2;
             fs.writeFile('Raspberry/raspberry-web/index.html', htmlContent, function (err) {

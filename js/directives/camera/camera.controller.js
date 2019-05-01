ca6f98c00342ae95c4bf58f68719552417b0bad3
Alois Aebischer <alois.aebischer24@gmail.com>
2019-05-01 11:37:08 +0200

    Camera size Choose the right camera to record

--- js/directives/camera/camera.controller.js
+++ js/directives/camera/camera.controller.js
@@ -6,15 +6,15 @@
 
     function cameraCtrl($scope) {
         var vm = this,
-        video = document.getElementsByClassName('cameraGear'),
-        vendorURL = window.URL || window.webkitURL,
-        record, recordedVideo="", recordedObject="",
-        date = new Date(), month, minutes, fileName;
+            video = document.getElementsByClassName('cameraGear'),
+            vendorURL = window.URL || window.webkitURL,
+            record, recordedVideo = "", recordedObject = "",
+            date = new Date(), month, minutes, fileName;
 
         navigator.getMedia = navigator.getUserMedia ||
-                            navigator.webkitGetUserMedia ||
-                            navigator.mozWetUserMedia ||
-                            navigator.msGetUserMedia;
+            navigator.webkitGetUserMedia ||
+            navigator.mozWetUserMedia ||
+            navigator.msGetUserMedia;
 
         function successCallback(stream) {
             var options = {
@@ -25,66 +25,65 @@
             video[1].src = vendorURL.createObjectURL(stream);
             record = recordRTC(stream, options);
         }
-        
+
         function errorCallback(error) {
-            console.log("No cam detected, maybe another application is using the device"); 
+            console.log("No cam detected, maybe another application is using the device");
         }
-        
+
         var mediaConstraints = {
             audio: false,
-            video: {
-                optional: [{sourceId: "e9be50686475f23e5c144e6319cb101a02f43b2e2e79fe1bde0441f7288b07fe"}]
-            }
+            video: true
         };
-        
-        navigator.getMedia(mediaConstraints, successCallback, errorCallback);
+
+
+        setTimeout(() => { navigator.getMedia(mediaConstraints, successCallback, errorCallback); }, 5000);
 
         $scope.rec = function () {
-            if($scope.cameraRecMenu==false){
+            if ($scope.cameraRecMenu == false) {
                 navigator.getMedia(mediaConstraints, successCallback, errorCallback);
-                $scope.cameraRecMenu=true;
+                $scope.cameraRecMenu = true;
             }
-            else if($scope.cameraRec==false){
+            else if ($scope.cameraRec == false) {
                 record.clearRecordedData();
-                $scope.cameraRec=true;
+                $scope.cameraRec = true;
                 record.startRecording();
             }
-            else{
-                $scope.cameraRec=false;
+            else {
+                $scope.cameraRec = false;
                 record.stopRecording(function (videoURL) {
-                    recordedObject=record;
-                    recordedVideo=videoURL;
+                    recordedObject = record;
+                    recordedVideo = videoURL;
                 });
                 navigator.getMedia(mediaConstraints, successCallback, errorCallback);
             }
         }
 
         $scope.save = function () {
-            if(recordedObject != "" && !$scope.cameraRec){
-                month=date.getMonth()+1;
-                minutes=date.getMinutes();
-                if(minutes < 10){
+            if (recordedObject != "" && !$scope.cameraRec) {
+                month = date.getMonth() + 1;
+                minutes = date.getMinutes();
+                if (minutes < 10) {
                     minutes = "0" + minutes;
                 }
-                fileName = date.getFullYear() + "/" + month + "/" + date.getDate() + "/" + date.getHours() + "h" + minutes ;
+                fileName = date.getFullYear() + "/" + month + "/" + date.getDate() + "/" + date.getHours() + "h" + minutes;
                 recordedObject.save(fileName);
             }
         }
         $scope.play = function () {
-            if(recordedVideo != "" && !$scope.cameraRec){
-                if($scope.cameraRecMenu==true){
-                    $scope.cameraPlay=true;
-                    $scope.cameraRecMenu=false;
+            if (recordedVideo != "" && !$scope.cameraRec) {
+                if ($scope.cameraRecMenu == true) {
+                    $scope.cameraPlay = true;
+                    $scope.cameraRecMenu = false;
                     video[0].src = recordedVideo;
                     video[1].src = recordedVideo;
                 }
-                else if($scope.cameraPlay==true){
-                    $scope.cameraPlay=false;
+                else if ($scope.cameraPlay == true) {
+                    $scope.cameraPlay = false;
                     video[0].pause();
                     video[1].pause();
                 }
-                else{
-                    $scope.cameraPlay=true;
+                else {
+                    $scope.cameraPlay = true;
                     video[0].play();
                     video[1].play();
                 }
